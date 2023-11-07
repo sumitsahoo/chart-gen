cf3bf9261f81e9fc11b9f4b0937d5c7a43bd33eb
Sumit Sahoo <sumitsahoo1988@gmail.com>
2023-11-07 22:07:45 +0530

    update assistant

--- src/util/chart_util.py
+++ src/util/chart_util.py
@@ -5,7 +5,8 @@ class ChartUtil:
     def __init__(self):
         self.client = OpenAI()
         self.assistant = self.client.beta.assistants.create(
-            instructions="You are a personal assistant called 'ChartGen'. When asked to create a chart, write and run code to generate the chart.",
+            name="ChartGen",
+            instructions="You are great at creating beautiful data visualizations. You analyze data and come up with data visualizations relevant to the data. You also share a brief text summary of the data visualization.",
             model="gpt-4-1106-preview",
             tools=[{"type": "code_interpreter"}],
         )
